7f7eddccdbe214862a2f2ec74bc72531d60d4c98
Luong <3q04032002@gmail.com>
2023-06-19 16:00:43 +0700

    re-split data

--- genre_classification/main.py
+++ genre_classification/main.py
@@ -7,24 +7,24 @@ import os
 
 if __name__ == '__main__':
     
-    data_path = 'data/data.csv'
-    tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
-    train_loader, valid_loader = get_dataloader(data_path, tokenizer=tokenizer)
-    criterion = torch.nn.CrossEntropyLoss()
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = RobertaClassification()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
-    epochs = 10
-    save_path = '/results'
+    # data_path = 'data/data.csv'
+    # tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+    # train_loader, valid_loader = get_dataloader(data_path, tokenizer=tokenizer)
+    # criterion = torch.nn.CrossEntropyLoss()
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # model = RobertaClassification()
+    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    # epochs = 10
+    save_path = './results'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
         
-    run_train(model,
-              train_loader,
-              valid_loader,
-              criterion,
-              optimizer,
-              device,
-              epochs,
-              save_path
-              )
+    # run_train(model,
+    #           train_loader,
+    #           valid_loader,
+    #           criterion,
+    #           optimizer,
+    #           device,
+    #           epochs,
+    #           save_path
+    #           )
