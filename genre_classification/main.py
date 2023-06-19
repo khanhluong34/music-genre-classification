c2c94391cc3691f3fabae7b7319d062e09e3f5fa
Luong <3q04032002@gmail.com>
2023-06-19 14:17:14 +0700

    add new model

--- genre_classification/main.py
+++ genre_classification/main.py
@@ -1,4 +1,4 @@
-from model import BERT_BiLSTM, BERTClassification
+from model import BERT_BiLSTM, BERTClassification, DistilBERTClassification
 from train import run_train 
 from utils import get_dataloader
 from transformers import DistilBertTokenizer
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     train_loader, valid_loader, test_loader = get_dataloader(data_path, tokenizer=tokenizer)
     criterion = torch.nn.CrossEntropyLoss()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = BERTClassification()
+    model = DistilBERTClassification()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
     epochs = 10
     save_path = '/results'
